cd86664f8fb02923ed5659b2434143ce2bad0666
VidhanThakur09 <vidhanthakur2700@gmail.com>
2025-05-16 15:13:14 +0530

    added navbar footer openac section

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
-
+import React from "react";
+import '/public/css/Navbar.css';
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <nav
+        className="navbar fixed-top navbar-expand-lg border"
+        style={{ backgroundColor: "white" }}
+      >
+        <div className="container px-3">
+          <a className="navbar-brand " href="#">
+            <img src="images/logo.svg" style={{ width: "25%" }} alt="logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <form className="d-flex" role="search">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Signup
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Products
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Support
+                  </a>
+                </li>
+                <i className="fa fa-bars burger" aria-hidden="true"></i>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function OpenAccount() {
   return (
-    <div>OpenAccount</div>
-  )
+    <div className="container p-5 mb-5" style={{ color: "#424242" }}>
+      <div className="row d-flex justify-content-center">
+        <h2 className=" text-center mt-4 title" style={{"fontWeight":"500","fontSize":"2rem"}}>Open a Zerodha account</h2>
+        <p className="text-center my-3 subtitle">
+          Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+          F&O trades.
+        </p>
+        <button className="mt-3 mb-5 btn fs-5 heroBtn ">
+          Sign up for free
+        </button>
+      </div>
+    </div>
+  );
 }
