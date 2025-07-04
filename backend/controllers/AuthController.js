4da96c2b11cc9399dec305ff9ea8e6904e40db3d
VidhanThakur09 <vidhanthakur2700@gmail.com>
2025-07-04 13:15:26 +0530

    fix:- routing ports 2

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -13,7 +13,9 @@ module.exports.Signup = async (req, res, next) => {
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: false,
+      httpOnly: true,
+      sameSite: 'None',
+      domain: '.onrender.com'
     });
     res
       .status(201)
@@ -41,9 +43,11 @@ module.exports.Login = async (req, res, next) => {
     }
      const token = createSecretToken(user._id);
      res.cookie("token", token, {
-       withCredentials: true,
-       httpOnly: false,
-     });
+      withCredentials: true,
+      httpOnly: true,
+      sameSite: 'None',
+      domain: '.onrender.com'
+    });
      res.status(201).json({ message: "User logged in successfully", success: true });
      next()
   } catch (error) {
